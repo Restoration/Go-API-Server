b806fcb686bbfd3d4a84c3dd73fd9019daf7cb35
RyotArch <r.yamamoto.system@gmail.com>
2019-11-02 05:28:05 +0900

    Add sample code

--- main.go
+++ main.go
@@ -1,170 +1,28 @@
-/*
 package main
 
 import (
-  "fmt"
-  "net/http"
-)
-
-func handler(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprintf(w, "Hello, World")
-}
-
-func main() {
-  http.HandleFunc("/", handler) // ハンドラを登録してウェブページを表示させる
-  http.ListenAndServe(":8080", nil)
-}
-*/
-/*
-Copyright 2014 Google Inc.
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-     http://www.apache.org/licenses/LICENSE-2.0
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
-// outyet is a web server that announces whether or not a particular Go version
-// has been tagged.
-package main
-
-import (
-	"expvar"
-	"flag"
 	"fmt"
-	"html/template"
-	"log"
+	"go-contacts/app"
 	"net/http"
-	"sync"
-	"time"
-        _ "github.com/go-sql-driver/mysql"
-        "database/sql"
-)
+	"os"
 
-// Command-line flags.
-var (
-	httpAddr   = flag.String("http", ":8080", "Listen address")
-	pollPeriod = flag.Duration("poll", 5*time.Second, "Poll period")
-	version    = flag.String("version", "1.4", "Go version")
+	"github.com/gorilla/mux"
 )
-
-const baseChangeURL = "https://go.googlesource.com/go/+/"
 
 func main() {
-	flag.Parse()
-	changeURL := fmt.Sprintf("%sgo%s", baseChangeURL, *version)
-	http.Handle("/", NewServer(*version, changeURL, *pollPeriod))
-	log.Fatal(http.ListenAndServe(*httpAddr, nil))
 
-        cnn, err := sql.Open("mysql", "docker:docker@tcp(db:5000)/test_db")
-        if err != nil {
-                log.Fatal(err)
-        }
+	router := mux.NewRouter()
+	router.Use(app.JwtAuthentication) //attach JWT auth middleware
 
-        id := 1
-        var name string
-
-        if err := cnn.QueryRow("SELECT name FROM test_tb WHERE id = ? LIMIT 1", id).Scan(&name); err != nil {
-                log.Fatal(err)
-        }
-        fmt.Println(id, name)
-}
-
-// Exported variables for monitoring the server.
-// These are exported via HTTP as a JSON object at /debug/vars.
-var (
-	hitCount       = expvar.NewInt("hitCount")
-	pollCount      = expvar.NewInt("pollCount")
-	pollError      = expvar.NewString("pollError")
-	pollErrorCount = expvar.NewInt("pollErrorCount")
-)
-
-// Server implements the outyet server.
-// It serves the user interface (it's an http.Handler)
-// and polls the remote repository for changes.
-type Server struct {
-	version string
-	url     string
-	period  time.Duration
-
-	mu  sync.RWMutex // protects the yes variable
-	yes bool
-}
-
-// NewServer returns an initialized outyet server.
-func NewServer(version, url string, period time.Duration) *Server {
-	s := &Server{version: version, url: url, period: period}
-	go s.poll()
-	return s
-}
-
-// poll polls the change URL for the specified period until the tag exists.
-// Then it sets the Server's yes field true and exits.
-func (s *Server) poll() {
-	for !isTagged(s.url) {
-		pollSleep(s.period)
+	port := os.Getenv("PORT") //Get port from .env file, we did not specify any port so this should return an empty string when tested locally
+	if port == "" {
+		port = "8000" //localhost
 	}
-	s.mu.Lock()
-	s.yes = true
-	s.mu.Unlock()
-	pollDone()
-}
 
-// Hooks that may be overridden for integration tests.
-var (
-	pollSleep = time.Sleep
-	pollDone  = func() {}
-)
+	fmt.Println(port)
 
-// isTagged makes an HTTP HEAD request to the given URL and reports whether it
-// returned a 200 OK response.
-func isTagged(url string) bool {
-	pollCount.Add(1)
-	r, err := http.Head(url)
+	err := http.ListenAndServe(":"+port, router) //Launch the app, visit localhost:8000/api
 	if err != nil {
-		log.Print(err)
-		pollError.Set(err.Error())
-		pollErrorCount.Add(1)
-		return false
-	}
-	return r.StatusCode == http.StatusOK
-}
-
-// ServeHTTP implements the HTTP user interface.
-func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	hitCount.Add(1)
-	s.mu.RLock()
-	data := struct {
-		URL     string
-		Version string
-		Yes     bool
-	}{
-		s.url,
-		s.version,
-		s.yes,
-	}
-	s.mu.RUnlock()
-	err := tmpl.Execute(w, data)
-	if err != nil {
-		log.Print(err)
+		fmt.Print(err)
 	}
 }
-
-// tmpl is the HTML template that drives the user interface.
-var tmpl = template.Must(template.New("tmpl").Parse(`
-<!DOCTYPE html><html><body><center>
-	<h2>Is Go {{.Version}} out yet?</h2>
-	<h1>
-  {{.name}}
-	{{if .Yes}}
-		<a href="{{.URL}}">YE!</a>
-	{{else}}
-		No. :-(
-	{{end}}
-	</h1>
-</center></body></html>
-`))
